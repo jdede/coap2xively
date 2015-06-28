9ab9e26591df40354d5ebc10c208324358d4039f
Jens Dede <jd@comnets.uni-bremen.de>
2015-06-28 18:52:35 +0200

    add one small comment

--- prepare-venv.sh
+++ prepare-venv.sh
@@ -9,6 +9,9 @@ virtualenv venv
 . ./venv/bin/activate
 echo "Installing in Virtualenv \"$VIRTUAL_ENV\""
 pip install twisted txthings
+
+# If the following line causes an error: remove "--pre" and remove the "venv"
+# directory
 pip install --pre xively-python
 
 deactivate
